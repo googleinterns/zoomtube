195b04cf40059126d4c5a4ff881d55de6292782f
n-wach <nmwachholz@gmail.com>
2020-07-17 23:34:48 +0000

    Fix formatting to pass linter

--- src/main/java/com/googleinterns/zoomtube/servlets/LectureServlet.java
+++ src/main/java/com/googleinterns/zoomtube/servlets/LectureServlet.java
@@ -15,19 +15,17 @@
 package com.googleinterns.zoomtube.servlets;
 
 import java.io.IOException;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.ServletException;
-
 
 /**
  * Provides information on a lecture.
  */
 @WebServlet("/lecture")
 public class LectureServlet extends HttpServlet {
-
   @Override
   public void init() throws ServletException {
     // TODO: Implement LectureServlet.
