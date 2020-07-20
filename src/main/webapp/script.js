c40d7e5ff2f8c98628443d25e02b58a3b8e604ce
Jay Saleh <jaysaleh66@gmail.com>
2020-07-20 16:58:54 +0000

    Added period

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -23,7 +23,7 @@ function createLectureListItem(lecture) {
   const listItem = document.createElement('li');
   const lectureLink = document.createElement('a');
   
-  // TODO: Redirect to specific lecture page/
+  // TODO: Redirect to specific lecture page.
   lectureLink.href = lecture.videoUrl;
   lectureLink.target = "_blank";
   lectureLink.innerText = lecture.lectureName;
