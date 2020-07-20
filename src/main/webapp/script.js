46daf02163c369900b0d6d1748a07cb8eee37645
Jay Saleh <jaysaleh66@gmail.com>
2020-07-20 19:01:35 +0000

    Added comments

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -1,13 +1,22 @@
+/** Used for accessing form input values. */
+const LECTURE_FORM = 'lectureForm';
+const NAME_INPUT = 'name-input';
+const VIDEO_INPUT = 'video-input';
+
+/** Used to send an error message to client. */
+const LECTURE_NAME_ERROR = 'Lecture name must be filled out.';
+const VIDEO_URL_ERROR = 'Video url must be filled out.';
+
 /** Checks if form is filled entirely, sends an alert if not. */
 function validateForm() {
-  var lectureName = document.forms['lectureForm']['name-input'].value;
-  var videoUrl = document.forms['lectureForm']['video-input'].value;
+  var lectureName = document.forms[LECTURE_FORM][NAME_INPUT].value;
+  var videoUrl = document.forms[LECTURE_FORM][VIDEO_INPUT].value;
   if (lectureName == null || lectureName == '') {
-    alert('Lecture name must be filled out.');
+    alert(LECTURE_NAME_ERROR);
     return false;
   }
   if (videoUrl == null || videoUrl == '') {
-    alert('Video url must be filled out.');
+    alert(VIDEO_URL_ERROR);
     return false;
   }
 }
