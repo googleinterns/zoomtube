ebd46048450000f944fb12e1914d6679ce3f1f86
Jay Saleh <jaysaleh66@gmail.com>
2020-07-20 19:03:22 +0000

    Fixed comment

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -7,7 +7,7 @@ const VIDEO_INPUT = 'video-input';
 const LECTURE_NAME_ERROR = 'Lecture name must be filled out.';
 const VIDEO_URL_ERROR = 'Video url must be filled out.';
 
-/** Checks if form is filled entirely, sends an alert if not. */
+/** Checks if lecture form is filled entirely, sends an alert if not. */
 function validateForm() {
   var lectureName = document.forms[LECTURE_FORM][NAME_INPUT].value;
   var videoUrl = document.forms[LECTURE_FORM][VIDEO_INPUT].value;
