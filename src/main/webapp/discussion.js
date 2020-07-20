c16c49ba7f3c3b57ebe0cd79c01ff5b5b81a40b2
n-wach <nmwachholz@gmail.com>
2020-07-20 23:23:55 +0000

    Fetch and display discussion on page load

---
+++ src/main/webapp/discussion.js
@@ -0,0 +1,50 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+const ENDPOINT = '/discussion';
+const PARAM_LECTURE = 'lecture';
+
+/**
+ * Adds comments to the {@code discussionElement}.
+ */
+async function loadDiscussion(discussionElement, lectureKey) {
+  const comments = await fetchDiscussion(lectureKey);
+  for (const comment of comments) {
+    discussionElement.appendChild(createComment(comment));
+  }
+}
+
+/**
+ * Requests all comments in the lecture specified by {@code lectureKey} from
+ * the {@link java.com.googleinterns.zoomtube.servlets.DiscussionServlet}.
+ */
+async function fetchDiscussion(lectureKey) {
+  const url = new URL(ENDPOINT, window.location.origin);
+  url.searchParams.append(PARAM_LECTURE, lectureKey);
+
+  const request = await fetch(url);
+  return request.json();
+}
+
+/**
+ * Creates an element for displaying a {@code comment}.
+ */
+function createComment(comment) {
+  const element = document.createElement('li');
+
+  // TODO: Display more than just the content.
+  element.innerText = comment.content;
+
+  return element;
+}
