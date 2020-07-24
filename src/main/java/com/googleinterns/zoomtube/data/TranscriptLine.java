c71f8c83e751b47d971f86239f9aa3c0fb6df5e6
Amy Nguyen <amytn@google.com>
2020-07-24 20:43:44 +0000

    Address review comment.

--- src/main/java/com/googleinterns/zoomtube/data/TranscriptLine.java
+++ src/main/java/com/googleinterns/zoomtube/data/TranscriptLine.java
@@ -49,8 +49,7 @@ public abstract class TranscriptLine {
   public abstract String content();
 
   /**
-   * Creates a {@code TranscriptLine} from a datastore {@link
-   * com.google.appengine.api.datastore.Entity} using the property names defined in this class.
+   * Creates a {@code TranscriptLine} from a datastore {@code entity} using the property names defined in this class.
    */
   public static TranscriptLine fromLineEntity(Entity entity) {
     Key key = entity.getKey();
