12e776af3703ee1c60f9f88590d00a0c605ea30a
n-wach <nmwachholz@gmail.com>
2020-07-29 20:49:51 +0000

    Cleanup tests

--- src/test/java/com/googleinterns/zoomtube/servlets/DiscussionServletTest.java
+++ src/test/java/com/googleinterns/zoomtube/servlets/DiscussionServletTest.java
@@ -2,6 +2,10 @@ package com.googleinterns.zoomtube.servlets;
 
 import static com.google.appengine.api.datastore.FetchOptions.Builder.withLimit;
 import static com.google.common.truth.Truth.assertThat;
+import static org.mockito.Mockito.anyString;
+import static org.mockito.Mockito.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
@@ -18,25 +22,45 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 import com.googleinterns.zoomtube.data.Comment;
-import com.googleinterns.zoomtube.mocks.MockRequest;
-import com.googleinterns.zoomtube.mocks.MockResponse;
 import com.ryanharter.auto.value.gson.GenerateTypeAdapter;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringReader;
+import java.io.StringWriter;
 import java.lang.reflect.Type;
 import java.time.Clock;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
 
 public class DiscussionServletTest {
+  private static final int LECTURE_A = 1;
+  private static final int LECTURE_B = 2;
+  private static final int LECTURE_C = 3;
+  private static final String LECTURE_A_STR = "1";
+  private static final String LECTURE_B_STR = "2";
+  private static final String LECTURE_C_STR = "3";
+  private static final int PARENT_ID = 32;
+  private static final String PARENT_ID_STR = "32";
+  private static final String CONTENT = "Test content";
+  private static final String EMAIL = "test@example.com";
+  private static final String AUTH_DOMAIN = "example.com";
+
+  @Rule public final MockitoRule mockito = MockitoJUnit.rule();
+  @Mock private HttpServletRequest request;
+  @Mock private HttpServletResponse response;
   private DiscussionServlet servlet;
-  private MockRequest request;
-  private MockResponse response;
   private LocalServiceTestHelper testServices = new LocalServiceTestHelper(
       new LocalUserServiceTestConfig(), new LocalDatastoreServiceTestConfig());
   private DatastoreService datastore;
@@ -44,10 +68,10 @@ public class DiscussionServletTest {
   @Before
   public void setUp() throws ServletException {
     testServices.setUp();
+    testServices.setEnvEmail(EMAIL);
+    testServices.setEnvAuthDomain(AUTH_DOMAIN);
     servlet = new DiscussionServlet();
     servlet.init();
-    response = new MockResponse();
-    request = new MockRequest();
     datastore = DatastoreServiceFactory.getDatastoreService();
   }
 
@@ -59,35 +83,28 @@ public class DiscussionServletTest {
   @Test
   public void doPost_loggedOut_postForbidden() throws ServletException, IOException {
     testServices.setEnvIsLoggedIn(false);
-    String LECTURE_ID = "1";
-    request.setParameter(DiscussionServlet.PARAM_LECTURE, LECTURE_ID);
+    when(request.getParameter(DiscussionServlet.PARAM_LECTURE)).thenReturn(LECTURE_A_STR);
 
     servlet.doPost(request, response);
 
-    assertThat(response.getStatus()).isEqualTo(HttpServletResponse.SC_FORBIDDEN);
+    verify(response).sendError(eq(HttpServletResponse.SC_FORBIDDEN), anyString());
     assertThat(datastore.prepare(new Query(Comment.ENTITY_KIND)).countEntities(withLimit(1)))
         .isEqualTo(0);
   }
 
   @Test
   public void doPost_storesCommentWithProperties_noParent() throws ServletException, IOException {
-    int LECTURE_ID = 1;
-    String CONTENT = "Test content";
-    String EMAIL = "test@example.com";
     testServices.setEnvIsLoggedIn(true);
-    testServices.setEnvEmail(EMAIL);
-    testServices.setEnvAuthDomain("example.com");
-    request.setParameter(DiscussionServlet.PARAM_LECTURE, Integer.toString(LECTURE_ID));
-    request.setContent(CONTENT);
-    PreparedQuery query = datastore.prepare(new Query(Comment.ENTITY_KIND));
-    assertThat(query.countEntities(withLimit(2))).isEqualTo(0);
+    when(request.getParameter(DiscussionServlet.PARAM_LECTURE)).thenReturn(LECTURE_A_STR);
+    when(request.getReader()).thenReturn(new BufferedReader(new StringReader(CONTENT)));
 
     servlet.doPost(request, response);
 
-    assertThat(response.getStatus()).isEqualTo(HttpServletResponse.SC_ACCEPTED);
+    verify(response).setStatus(HttpServletResponse.SC_ACCEPTED);
+    PreparedQuery query = datastore.prepare(new Query(Comment.ENTITY_KIND));
     assertThat(query.countEntities(withLimit(2))).isEqualTo(1);
     Comment comment = Comment.fromEntity(query.asSingleEntity());
-    assertThat(comment.lecture().getId()).isEqualTo(LECTURE_ID);
+    assertThat(comment.lecture().getId()).isEqualTo(LECTURE_A);
     assertThat(comment.author().getEmail()).isEqualTo(EMAIL);
     assertThat(comment.content()).isEqualTo(CONTENT);
     assertThat(comment.parent().isPresent()).isFalse();
@@ -95,19 +112,15 @@ public class DiscussionServletTest {
 
   @Test
   public void doPost_storesCommentParent() throws ServletException, IOException {
-    int PARENT_ID = 32;
     testServices.setEnvIsLoggedIn(true);
-    testServices.setEnvEmail("test@example.com");
-    testServices.setEnvAuthDomain("example.com");
-    request.setParameter(DiscussionServlet.PARAM_LECTURE, "1");
-    request.setParameter(DiscussionServlet.PARAM_PARENT, Integer.toString(PARENT_ID));
-    request.setContent("Test content");
-    PreparedQuery query = datastore.prepare(new Query(Comment.ENTITY_KIND));
-    assertThat(query.countEntities(withLimit(2))).isEqualTo(0);
+    when(request.getParameter(DiscussionServlet.PARAM_LECTURE)).thenReturn(LECTURE_A_STR);
+    when(request.getParameter(DiscussionServlet.PARAM_PARENT)).thenReturn(PARENT_ID_STR);
+    when(request.getReader()).thenReturn(new BufferedReader(new StringReader(CONTENT)));
 
     servlet.doPost(request, response);
 
-    assertThat(response.getStatus()).isEqualTo(HttpServletResponse.SC_ACCEPTED);
+    verify(response).setStatus(HttpServletResponse.SC_ACCEPTED);
+    PreparedQuery query = datastore.prepare(new Query(Comment.ENTITY_KIND));
     Comment comment = Comment.fromEntity(query.asSingleEntity());
     assertThat(comment.parent().isPresent()).isTrue();
     assertThat(comment.parent().get().getId()).isEqualTo(PARENT_ID);
@@ -115,49 +128,55 @@ public class DiscussionServletTest {
 
   @Test
   public void doGet_returnsNothingForUnknownLecture() throws ServletException, IOException {
-    request.setParameter(DiscussionServlet.PARAM_LECTURE, "1");
+    when(request.getParameter(DiscussionServlet.PARAM_LECTURE)).thenReturn(LECTURE_A_STR);
+    StringWriter content = new StringWriter();
+    PrintWriter writer = new PrintWriter(content);
+    when(response.getWriter()).thenReturn(writer);
 
     servlet.doGet(request, response);
 
-    assertThat(response.getContentType()).isEqualTo("application/json;");
-    assertThat(response.getContentAsString()).startsWith("[]");
+    verify(response).setContentType("application/json;");
+    List<Comment> comments = getCommentsFromJson(content.toString());
+    assertThat(comments.isEmpty()).isTrue();
   }
 
   @Test
   public void doGet_returnsComments() throws ServletException, IOException {
-    request.setParameter(DiscussionServlet.PARAM_LECTURE, "1");
-    datastore.put(createTestCommentEntity(1));
-    datastore.put(createTestCommentEntity(1));
-    datastore.put(createTestCommentEntity(1));
+    when(request.getParameter(DiscussionServlet.PARAM_LECTURE)).thenReturn(LECTURE_A_STR);
+    datastore.put(createTestCommentEntity(LECTURE_A));
+    datastore.put(createTestCommentEntity(LECTURE_A));
+    datastore.put(createTestCommentEntity(LECTURE_A));
+    StringWriter content = new StringWriter();
+    PrintWriter writer = new PrintWriter(content);
+    when(response.getWriter()).thenReturn(writer);
 
     servlet.doGet(request, response);
 
-    assertThat(response.getContentType()).isEqualTo("application/json;");
-    String json = response.getContentAsString();
-    Gson gson = new GsonBuilder().registerTypeAdapterFactory(GenerateTypeAdapter.FACTORY).create();
-    Type listType = new TypeToken<ArrayList<Comment>>() {}.getType();
-    List<Comment> comments = gson.fromJson(json, listType);
+    verify(response).setContentType("application/json;");
+    List<Comment> comments = getCommentsFromJson(content.toString());
     assertThat(comments.size()).isEqualTo(3);
   }
 
   @Test
   public void doGet_returnsCommentsForSpecificLecture() throws ServletException, IOException {
-    request.setParameter(DiscussionServlet.PARAM_LECTURE, "2");
-    datastore.put(createTestCommentEntity(1));
-    datastore.put(createTestCommentEntity(1));
-    datastore.put(createTestCommentEntity(1));
-    datastore.put(createTestCommentEntity(2)); // this one
-    datastore.put(createTestCommentEntity(2)); // and this one
-    datastore.put(createTestCommentEntity(3));
-    datastore.put(createTestCommentEntity(5));
+    // Looking for two comments under LECTURE_B.
+    when(request.getParameter(DiscussionServlet.PARAM_LECTURE)).thenReturn(LECTURE_B_STR);
+    datastore.put(createTestCommentEntity(LECTURE_A));
+    datastore.put(createTestCommentEntity(LECTURE_C));
+    datastore.put(createTestCommentEntity(LECTURE_B)); // Add the first.
+    datastore.put(createTestCommentEntity(LECTURE_A));
+    datastore.put(createTestCommentEntity(LECTURE_A));
+    datastore.put(createTestCommentEntity(LECTURE_C));
+    datastore.put(createTestCommentEntity(LECTURE_B)); // Add the second.
+    datastore.put(createTestCommentEntity(LECTURE_C));
+    StringWriter content = new StringWriter();
+    PrintWriter writer = new PrintWriter(content);
+    when(response.getWriter()).thenReturn(writer);
 
     servlet.doGet(request, response);
 
-    assertThat(response.getContentType()).isEqualTo("application/json;");
-    String json = response.getContentAsString();
-    Gson gson = new GsonBuilder().registerTypeAdapterFactory(GenerateTypeAdapter.FACTORY).create();
-    Type listType = new TypeToken<ArrayList<Comment>>() {}.getType();
-    List<Comment> comments = gson.fromJson(json, listType);
+    verify(response).setContentType("application/json;");
+    List<Comment> comments = getCommentsFromJson(content.toString());
     assertThat(comments.size()).isEqualTo(2);
   }
 
@@ -172,5 +191,12 @@ public class DiscussionServletTest {
     commentEntity.setProperty(Comment.PROP_CREATED, new Date(Clock.systemUTC().millis()));
 
     return commentEntity;
+  }
+
+  private List<Comment> getCommentsFromJson(String json) {
+    Gson gson = new GsonBuilder().registerTypeAdapterFactory(GenerateTypeAdapter.FACTORY).create();
+    Type listType = new TypeToken<ArrayList<Comment>>() {}.getType();
+    List<Comment> comments = gson.fromJson(json, listType);
+    return comments;
   }
 }
