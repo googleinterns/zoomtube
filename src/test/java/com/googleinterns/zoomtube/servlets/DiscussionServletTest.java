f93e2571a5ebd260298a7b74aaded3182ca1e229
n-wach <nmwachholz@gmail.com>
2020-07-29 00:31:35 +0000

    Add more tests for discussions

--- src/test/java/com/googleinterns/zoomtube/servlets/DiscussionServletTest.java
+++ src/test/java/com/googleinterns/zoomtube/servlets/DiscussionServletTest.java
@@ -5,6 +5,8 @@ import static com.google.common.truth.Truth.assertThat;
 
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
 import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
 import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
@@ -24,8 +26,8 @@ public class DiscussionServletTest {
   private MockRequest request;
   private MockResponse response;
   private LocalServiceTestHelper testServices = new LocalServiceTestHelper(
-      new LocalUserServiceTestConfig(),
-      new LocalDatastoreServiceTestConfig().setDefaultHighRepJobPolicyUnappliedJobPercentage(100));
+      new LocalUserServiceTestConfig(), new LocalDatastoreServiceTestConfig());
+  private DatastoreService datastore;
 
   @Before
   public void setUp() throws ServletException {
@@ -34,6 +36,7 @@ public class DiscussionServletTest {
     servlet.init();
     response = new MockResponse();
     request = new MockRequest();
+    datastore = DatastoreServiceFactory.getDatastoreService();
   }
 
   @After
@@ -43,14 +46,67 @@ public class DiscussionServletTest {
 
   @Test
   public void doPost_loggedOut_postForbidden() throws ServletException, IOException {
-    // authService.setEnvIsLoggedIn(false);
+    testServices.setEnvIsLoggedIn(false);
     String LECTURE_ID = "1";
     request.setParameter(DiscussionServlet.PARAM_LECTURE, LECTURE_ID);
 
     servlet.doPost(request, response);
 
     assertThat(response.getStatus()).isEqualTo(HttpServletResponse.SC_FORBIDDEN);
-    DatastoreService ds = DatastoreServiceFactory.getDatastoreService();
-    assertThat(ds.prepare(new Query(Comment.ENTITY_KIND)).countEntities(withLimit(1))).isEqualTo(0);
+    assertThat(datastore.prepare(new Query(Comment.ENTITY_KIND)).countEntities(withLimit(1)))
+        .isEqualTo(0);
+  }
+
+  @Test
+  public void doPost_storesCommentWithProperties_noParent() throws ServletException, IOException {
+    int LECTURE_ID = 1;
+    String CONTENT = "Test content";
+    String EMAIL = "test@example.com";
+    testServices.setEnvIsLoggedIn(true);
+    testServices.setEnvEmail(EMAIL);
+    testServices.setEnvAuthDomain("example.com");
+    request.setParameter(DiscussionServlet.PARAM_LECTURE, Integer.toString(LECTURE_ID));
+    request.setContent(CONTENT);
+    PreparedQuery query = datastore.prepare(new Query(Comment.ENTITY_KIND));
+    assertThat(query.countEntities(withLimit(2))).isEqualTo(0);
+
+    servlet.doPost(request, response);
+
+    assertThat(response.getStatus()).isEqualTo(HttpServletResponse.SC_ACCEPTED);
+    assertThat(query.countEntities(withLimit(2))).isEqualTo(1);
+    Comment comment = Comment.fromEntity(query.asSingleEntity());
+    assertThat(comment.lecture().getId()).isEqualTo(LECTURE_ID);
+    assertThat(comment.author().getEmail()).isEqualTo(EMAIL);
+    assertThat(comment.content()).isEqualTo(CONTENT);
+    assertThat(comment.parent().isPresent()).isFalse();
+  }
+
+  @Test
+  public void doPost_storesCommentParent() throws ServletException, IOException {
+    int PARENT_ID = 32;
+    testServices.setEnvIsLoggedIn(true);
+    testServices.setEnvEmail("test@example.com");
+    testServices.setEnvAuthDomain("example.com");
+    request.setParameter(DiscussionServlet.PARAM_LECTURE, "1");
+    request.setParameter(DiscussionServlet.PARAM_PARENT, Integer.toString(PARENT_ID));
+    request.setContent("Test content");
+    PreparedQuery query = datastore.prepare(new Query(Comment.ENTITY_KIND));
+    assertThat(query.countEntities(withLimit(2))).isEqualTo(0);
+
+    servlet.doPost(request, response);
+
+    assertThat(response.getStatus()).isEqualTo(HttpServletResponse.SC_ACCEPTED);
+    Comment comment = Comment.fromEntity(query.asSingleEntity());
+    assertThat(comment.parent().isPresent()).isTrue();
+    assertThat(comment.parent().get().getId()).isEqualTo(PARENT_ID);
+  }
+
+  @Test
+  public void doGet_returnsNothingForUnknownLecture() throws ServletException, IOException {
+    request.setParameter(DiscussionServlet.PARAM_LECTURE, "1");
+
+    servlet.doGet(request, response);
+
+    assertThat(response.getContentAsString()).startsWith("[]");
   }
 }
