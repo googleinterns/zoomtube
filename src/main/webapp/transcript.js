1c7d9e6eac4dc98b3a06d737e60a1758e646299a
Amy Nguyen <amytn@google.com>
2020-07-30 16:08:59 +0000

    Make pretty.

--- src/main/webapp/transcript.js
+++ src/main/webapp/transcript.js
@@ -3,7 +3,7 @@
  */
 function sendPostToTranscript() {
   // TODO: Update with user input (window.location.search)
-  const testParamsString = "Obgnr9pc820";
+  const testParamsString = 'Obgnr9pc820';
   const params = new URLSearchParams(testParamsString);
   fetch('/transcript', {method: 'POST', body: params});
 }
