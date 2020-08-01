8b61cf5fd270cceabbaa1450dc35c16697a86816
Amy Nguyen <amytn@google.com>
2020-08-01 05:08:36 +0000

    Update hardcoded URL to use user input.

--- src/main/webapp/transcript.js
+++ src/main/webapp/transcript.js
@@ -2,8 +2,6 @@
  * Sends a POST request to the transcript.
  */
 function sendPostToTranscript() {
-  // TODO: Update with user input (window.location.search)
-  const testParamsString = 'id=123456789&video=Obgnr9pc820';
-  const params = new URLSearchParams(testParamsString);
+  const params = new URLSearchParams(window.location.search);
   fetch('/transcript', {method: 'POST', body: params});
 }
