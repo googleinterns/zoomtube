31d578c19cb9aa565e5e612c5fa5ac37edb7fcfd
jaysaleh <47684906+jaysaleh@users.noreply.github.com>
2020-08-04 15:11:29 -0700

    Adds video iFrame to lecture-view.html (#55)
    * Added port for viewing dev server
    
    * Added post
    
    * Added comment
    
    * Removed space
    
    * Removed exception
    
    * Link -> URL
    
    * Made pretty
    
    * Fixed comments
    
    * Added comments
    
    * Fixed comment
    
    * Added comment
    
    * Added compiledPattern to init
    
    * made pretty
    
    * Fixed chnages
    
    * Added submit text
    
    * Adds doGet() in LectureServlet.java (#12)
    
    * Implemented doGet
    
    * Changed entity name
    
    * Made pretty
    
    * Minor fix
    
    * Made changes
    
    * Added method in lecture.java
    
    * Adds lecture list to comments section of portfolio (#14)
    
    * Implemented doGet
    
    * Added list
    
    * Changed entity name
    
    * Added comments
    
    * Added period
    
    * Made pretty
    
    * Made pretty
    
    * Minor fix
    
    * Made changes
    
    * Added method in lecture.java
    
    * Made changes
    
    * Redirects user to lecture page and grabs lecture key from URL (#31)
    
    * Add README describing project (#4)
    
    * Add README stub
    
    * Add Technology/Usage sections to README
    
    * Added iFrame API
    
    * Update usage instructions
    
    * Update Design Doc link in README.md
    
    Co-authored-by: Amy Nguyen <35519361+amuamushu@users.noreply.github.com>
    
    * Add URLs and a few more APIs
    
    * Remove React from list of tools
    
    Co-authored-by: Jay Saleh <jaysaleh66@gmail.com>
    Co-authored-by: Amy Nguyen <35519361+amuamushu@users.noreply.github.com>
    
    * Add js and html files for lecture view and list
    
    * Added redirect
    
    * Removed file
    
    * Added periods
    
    * Added URL
    
    * Fixed comment
    
    * Changed key to id
    
    Co-authored-by: Nathan Wachholz <nmwachholz@gmail.com>
    Co-authored-by: Amy Nguyen <35519361+amuamushu@users.noreply.github.com>
    
    * inital commit
    
    * Fixed bug
    
    * Added url builder
    
    * Cleaned up code
    
    * Minor fix
    
    * Fixed comment
    
    * Fixed error
    
    * Added prop_id
    
    * Added URL builder to LectureServlet.java (#40)
    
    * Added url builder
    
    * Minor fix
    
    * Fixed comment
    
    * Fixed error
    
    * Added prop_id
    
    * Made fixes
    
    * Minor fixes
    
    * Added new linter
    
    * Added cosnt
    
    * Fixed id
    
    * Changed file
    
    * Fixed and added a todo
    
    * Added view.js
    
    * removed discussion
    
    * removed readme
    
    * removed comment
    
    * removed code owners
    
    * Fixed problem
    
    * Added file header
    
    * Fixed validate
    
    * Decomposed
    
    * Minor fixes
    
    * Made changes
    
    * Fixed comment
    
    * Small chnage
    
    * Fixed spacing
    
    * Avoids duplicate lectures (#66)
    
    * Fixed duplicate
    
    * Check database
    
    * made pretty
    
    * Fixed comment
    
    * made fixes
    
    * made chnages
    
    * Resolved comments
    
    * Validated
    
    * Modified comment
    
    Co-authored-by: Nathan Wachholz <nmwachholz@gmail.com>
    Co-authored-by: Amy Nguyen <35519361+amuamushu@users.noreply.github.com>

--- src/main/java/com/googleinterns/zoomtube/servlets/LectureServlet.java
+++ src/main/java/com/googleinterns/zoomtube/servlets/LectureServlet.java
@@ -66,6 +66,7 @@ public class LectureServlet extends HttpServlet {
 
   // TODO: Check if URL is valid.
   @Override
+  // TODO: Check if URL is valid.
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
     Optional<String> optionalVideoUrl = getParameter(request, LINK_INPUT);
     Optional<Entity> existingEntity = checkUrlInDatabase(optionalVideoUrl);
