70ca65ec561d5a21e446f824f2bee7c02d599835
Amy Nguyen <amytn@google.com>
2020-08-05 03:36:22 +0000

    Update method name in test file.

--- src/test/java/com/googleinterns/zoomtube/servlets/TranscriptServletTest.java
+++ src/test/java/com/googleinterns/zoomtube/servlets/TranscriptServletTest.java
@@ -238,7 +238,7 @@ public final class TranscriptServletTest {
       Entity lineEntity = new Entity(TranscriptLineUtil.KIND);
       lineEntity.setProperty(TranscriptLineUtil.LECTURE, lectureKey);
       // Set dummy values because AutoValue needs all the values to create a TranscriptLine object.
-      lineEntity.setProperty(TranscriptLineUtil.START, /* start= */ new Date());
+      lineEntity.setProperty(TranscriptLineUtil.START,  /* start= */ new Date());
       lineEntity.setProperty(TranscriptLineUtil.DURATION, /* duration= */ new Date());
       lineEntity.setProperty(TranscriptLineUtil.END, /* end= */ new Date());
       lineEntity.setProperty(TranscriptLineUtil.CONTENT, "test content");
