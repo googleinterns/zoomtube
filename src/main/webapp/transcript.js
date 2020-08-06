07537458b741c5f1d613dfefccdbe05dc501a3ed
Amy Nguyen <amytn@google.com>
2020-08-06 02:47:47 +0000

    Add header for transcript container.

--- src/main/webapp/transcript.js
+++ src/main/webapp/transcript.js
@@ -13,7 +13,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-const TRANSCRIPT_CONTAINER = 'transcript-container';
+const TRANSCRIPT_CONTAINER = 'transcript-lines-container';
 const ENDPOINT_TRANSCRIPT = '/transcript';
 
 /**
