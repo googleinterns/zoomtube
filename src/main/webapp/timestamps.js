51be1989a5fa307e5e38a9d5b84d257201ea2298
Amy Nguyen <amytn@google.com>
2020-08-07 21:40:32 +0000

    Address review comment.

--- src/main/webapp/timestamps.js
+++ src/main/webapp/timestamps.js
@@ -17,8 +17,8 @@
  */
 function timestampToString(timestamp) {
   const date = new Date(timestamp);
-  const seconds = date.getSeconds().toString().padStart(2, '0');
-  const minutes = date.getMinutes().toString().padStart(2, '0');
+  const seconds = date.getSeconds().toString().padStart(/* targetLength= */2, /* padString= */'0');
+  const minutes = date.getMinutes().toString().padStart(/* targetLength= */2, /* padString= */'0');
   if (date.getHours() == 0) {
     return `${minutes}:${seconds}`;
   }
