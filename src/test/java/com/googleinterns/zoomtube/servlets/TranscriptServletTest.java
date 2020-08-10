c8f2d626bd005595d96f3503f6c099e9bbbfa384
Amy Nguyen <amytn@google.com>
2020-08-10 18:41:10 +0000

    Restore TranscriptServlet files.

--- src/test/java/com/googleinterns/zoomtube/servlets/TranscriptServletTest.java
+++ src/test/java/com/googleinterns/zoomtube/servlets/TranscriptServletTest.java
@@ -23,6 +23,7 @@ import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.datastore.Query;
 import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
 import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
 import com.google.gson.Gson;
@@ -49,6 +50,10 @@ import org.junit.runners.JUnit4;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
+import com.google.appengine.api.datastore.Query;	
+import com.google.appengine.api.datastore.Query.Filter;	
+import com.google.appengine.api.datastore.Query.FilterOperator;	
+import com.google.appengine.api.datastore.Query.FilterPredicate;
 
 @RunWith(JUnit4.class)
 public final class TranscriptServletTest {
@@ -189,5 +194,20 @@ public final class TranscriptServletTest {
           /* start= */ 0F, /* duration= */ 0F, /* end= */ 0F);
       datastore.put(lineEntity);
     }
+  }
+
+  private int entitiesInDatastoreCount(long lectureId) {	
+    // A limit of 100 for the maximum number of entities counted is used because	
+    // we can assume that for this test datastore, there won't be more than 100 entities	
+    // for a lecture key.	
+    return datastore.prepare(filteredQueryOfTranscriptLinesByLectureId(lectureId))	
+        .countEntities(withLimit(100));	
+  }	
+
+  private Query filteredQueryOfTranscriptLinesByLectureId(long lectureId) {	
+    Key lectureKey = KeyFactory.createKey(LectureUtil.KIND, lectureId);	
+    Filter lectureFilter =	
+        new FilterPredicate(TranscriptLineUtil.LECTURE, FilterOperator.EQUAL, lectureKey);	
+    return new Query(TranscriptLineUtil.KIND).setFilter(lectureFilter);	
   }
 }
