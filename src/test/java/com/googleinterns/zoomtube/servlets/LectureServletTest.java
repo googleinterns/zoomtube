650ddda5062e566b5ef89e16c227035a26e368ad
Jay Saleh <jaysaleh66@gmail.com>
2020-08-11 18:51:24 +0000

    Removed slash

--- src/test/java/com/googleinterns/zoomtube/servlets/LectureServletTest.java
+++ src/test/java/com/googleinterns/zoomtube/servlets/LectureServletTest.java
@@ -92,7 +92,7 @@ public final class LectureServletTest {
     servlet.doPost(request, response);
 
     assertThat(datastoreService.prepare(new Query(LectureUtil.KIND)).countEntities()).isEqualTo(1);
-    verify(response).sendRedirect("/view/?id=1");
+    verify(response).sendRedirect("/view?id=1");
   }
 
   @Test
@@ -104,7 +104,7 @@ public final class LectureServletTest {
     servlet.doPost(request, response);
 
     assertThat(datastoreService.prepare(new Query(LectureUtil.KIND)).countEntities()).isEqualTo(1);
-    verify(response).sendRedirect("/view/?id=1");
+    verify(response).sendRedirect("/view?id=1");
   }
 
   @Test
