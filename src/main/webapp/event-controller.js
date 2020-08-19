fa8d44eadc2d63f56eb10edbfac441e2c7e14ad6
Jay Saleh <jaysaleh66@gmail.com>
2020-08-19 01:25:06 +0000

    Added callback class

---
+++ src/main/webapp/event-controller.js
@@ -0,0 +1,51 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/** Manages storing eventListeners and broadcasting events. */
+class EventController {
+  constructor() {
+    this.eventListeners = new Map();
+    this.addEventListener = this.addEventListener.bind(this);
+    this.broadcastEvent = this.broadcastEvent.bind(this);
+  }
+
+  /** Adds `callBack` to `eventNames` stores in `eventListeners`. */
+  addEventListener(callBack, ...eventNames) {
+    for (eventName of eventNames) {
+      let callBacks = [];
+      if (this.eventListeners.has(eventName)) {
+        callBacks = this.listeners.get(eventName);
+      }
+      callBacks.push(callBack);
+      this.eventListeners.set(eventName, callBacks);
+    }
+  }
+
+  /**
+   *  Calls all callbacks associated with `eventName` and
+   *  passes `params` to callback. Returns false if there
+   *  were no callbacks, true otherwise.
+   */
+  broadcastListener(eventName, ...params) {
+    if (!this.eventListeners.has(eventName)) {
+      console.error('No event listeners found.');
+      return false;
+    }
+    const callBacks = this.listeners.get(eventName);
+    callBacks.forEach((callback) => {
+      callback(...params);
+    });
+    return true;
+  }
+}
