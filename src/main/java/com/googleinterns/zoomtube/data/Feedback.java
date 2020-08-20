87d2d5be5ee937fdc76bde964236a80bf6d89375
Jay Saleh <jaysaleh66@gmail.com>
2020-08-20 22:11:23 +0000

    Removed @generatetypeadapter

--- src/main/java/com/googleinterns/zoomtube/data/Feedback.java
+++ src/main/java/com/googleinterns/zoomtube/data/Feedback.java
@@ -16,10 +16,8 @@ package com.googleinterns.zoomtube.data;
 
 import com.google.appengine.api.datastore.Key;
 import com.google.auto.value.AutoValue;
-import com.ryanharter.auto.value.gson.GenerateTypeAdapter;
 
 /** Contains data related to feedback left by clicking icons. */
-@GenerateTypeAdapter
 @AutoValue
 public abstract class Feedback {
   public static enum Type {
@@ -28,7 +26,7 @@ public abstract class Feedback {
     TOO_FAST,
     TOO_SLOW,
   }
-  
+
   /** Returns feedback's datastore entity key. */
   public abstract Key feedbackKey();
 
