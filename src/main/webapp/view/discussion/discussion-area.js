5010ed006600d6682fd10afca0004777961ba085
Nathan Wachholz <nmwachholz@gmail.com>
2020-08-21 13:51:32 -0700

    Create DiscussionArea javascript class (#261)
    * Added module to index.html
    
    * Fixed module.
    
    * allowed video to play
    
    * Removed export statements from bottom of the file
    
    * Added new line to fix linter
    
    * Eliminated some unused vars
    
    * Elimated no unused vars
    
    * Remvoed newline in view.js
    
    * Add babel-eslint as parser
    
    * Start
    
    * Finish manager
    
    * Add new comment onclick
    
    * Minor fixes
    
    * Use correct lecture ID
    
    * Reload on post
    
    * Await postRoot
    
    * Fix await
    
    * Start making class
    
    * Move to new file
    
    * Make pretty
    
    * Move to new file
    
    * Rename file
    
    * Add JSDoc
    
    * Add JSDoc
    
    * Add type annotation
    
    * Fix minor typos
    
    * Clarify JSDoc
    
    * Add TODO for postReply click
    
    * Fix typo
    
    * Update constructor JSDoc
    
    * Add parens for clarity
    
    * Update JSDoc
    
    Co-authored-by: Jay Saleh <jaysaleh66@gmail.com>
    Co-authored-by: jaysaleh <47684906+jaysaleh@users.noreply.github.com>

---
+++ src/main/webapp/view/discussion/discussion-area.js
@@ -0,0 +1,129 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import {timestampToString} from '../../timestamps.js';
+import DiscussionManager from './discussion-manager.js';
+import {COMMENT_TYPE_QUESTION} from './discussion.js';
+import {DiscussionComment} from './discussion.js';
+
+export const ELEMENT_DISCUSSION =
+    document.querySelector('#discussion-comments');
+
+/*
+ * Displays the entire Discussion Area UI, and implements posting
+ * new comments and loading existing ones to the current lecture.
+ */
+export default class DiscussionArea {
+  static #ELEMENT_POST_TEXTAREA = document.querySelector('#post-textarea');
+  static #ELEMENT_TIMESTAMP_SPAN = document.querySelector('#timestamp-span');
+  static #TIME_TOLERANCE_MS = 10000;  // 10 seconds.
+  #lecture;
+  #manager;
+  #currentTimeMs;
+  #currentRootCommentElements;
+
+  /**
+   * Creates a `DiscussionArea` for a `lecture`.
+   */
+  constructor(lecture) {
+    this.#lecture = lecture;
+    this.#manager = new DiscussionManager(this.#lecture);
+    this.#currentTimeMs = 0;
+    this.#currentRootCommentElements = [];
+  }
+
+  /**
+   * Initialize the discussion area by loading the current comments.
+   */
+  async initialize() {
+    await this.loadDiscussion();
+  }
+
+  /**
+   * Fetches and displays the current comments.
+   */
+  async loadDiscussion() {
+    // Clear any existing comments before loading.
+    ELEMENT_DISCUSSION.textContent = '';
+    this.#currentRootCommentElements = [];
+
+    const rootComments = await this.#manager.fetchRootComments();
+    for (const rootComment of rootComments) {
+      const rootCommentElement = new DiscussionComment(rootComment, this);
+      this.#currentRootCommentElements.push(rootCommentElement);
+      ELEMENT_DISCUSSION.appendChild(rootCommentElement);
+    }
+  }
+
+  /**
+   * Returns an array of the `DiscussionComment`s with timestamps near
+   * `timestampMs`. This returns an empty array if no elements are nearby.
+   *
+   * <p>A comment is nearby if it is within `TIME_TOLERANCE_MS`.
+   */
+  getNearbyDiscussionComments(timestampMs) {
+    const nearby = [];
+    // currentRootCommentElements is already sorted by timestamp.
+    for (const element of this.#currentRootCommentElements) {
+      const commentTime = element.comment.timestampMs.value;
+      if (commentTime < (timestampMs - DiscussionArea.#TIME_TOLERANCE_MS)) {
+        // Before the start of the range, continue to next.
+        continue;
+      }
+      if (commentTime > (timestampMs + DiscussionArea.#TIME_TOLERANCE_MS)) {
+        // Outside of range, there will be no more.
+        return nearby;
+      }
+      nearby.push(element);
+    }
+    return nearby;
+  }
+
+  /**
+   * Seeks the discussion area to `timeMs`. This involves scrolling the
+   * comments, and updating the time displayed in the new comment area.
+   */
+  seek(timeMs) {
+    this.#currentTimeMs = timeMs;
+    DiscussionArea.#ELEMENT_TIMESTAMP_SPAN.innerText =
+        timestampToString(timeMs);
+    const nearbyComments = this.getNearbyDiscussionComments(timeMs);
+    if (nearbyComments.length == 0) {
+      return;
+    }
+    nearbyComments[0].scrollToTopOfDiscussion();
+  }
+
+  /**
+   * Posts the comment in the new comment area, and reloads the discussion.
+   */
+  postNewComment() {
+    this.#manager
+        .postRootComment(
+            DiscussionArea.#ELEMENT_POST_TEXTAREA.value, this.#currentTimeMs,
+            COMMENT_TYPE_QUESTION)
+        .then(() => {
+          this.loadDiscussion();
+        });
+  }
+
+  /**
+   * Posts `content` as a reply to `parentId`, and reloads the discussion.
+   */
+  postReply(content, parentId) {
+    this.#manager.postReply(content, parentId).then(() => {
+      this.loadDiscussion();
+    });
+  }
+}
--- src/main/webapp/view/discussion/discussion.js
+++ src/main/webapp/view/discussion/discussion.js
@@ -14,15 +14,11 @@
 
 import {timestampToString} from '../../timestamps.js';
 
-import DiscussionManager from './discussion-manager.js';
+import DiscussionArea, {ELEMENT_DISCUSSION} from './discussion-area.js';
 
 export const COMMENT_TYPE_REPLY = 'REPLY';
 export const COMMENT_TYPE_QUESTION = 'QUESTION';
 export const COMMENT_TYPE_NOTE = 'NOTE';
-
-const ELEMENT_DISCUSSION = document.querySelector('#discussion-comments');
-const ELEMENT_POST_TEXTAREA = document.querySelector('#post-textarea');
-const ELEMENT_TIMESTAMP_SPAN = document.querySelector('#timestamp-span');
 
 const TEMPLATE_COMMENT = document.querySelector('#comment-template');
 
@@ -36,91 +32,25 @@ const SELECTOR_CANCEL_REPLY = '#cancel-reply';
 const SELECTOR_POST_REPLY = '#post-reply';
 const SELECTOR_REPLY_TEXTAREA = '#reply-textarea';
 
-// 10 seconds.
-const TIME_TOLERANCE_MS = 10000;
-
-// TODO: Refactor these global variables into a namespace, module, or class.
-// See: #191.
-let newCommentTimestampMs = 0;
-let /** !Array<DiscussionComment> */ currentRootDiscussionComments = [];
+export let /** DiscussionArea */ discussion;
 
 /**
- * Loads the lecture disucssion.
+ * Loads the lecture discussion.
  */
 export async function intializeDiscussion() {
-  await loadDiscussion();
-}
-
-// This is used as the `onclick` handler of the new comment area submit button.
-window.postNewComment = () => {
-  const manager = new DiscussionManager(window.LECTURE);
-  // TODO: Add support for submitting types other than QUESTION.
-  manager
-      .postRootComment(
-          ELEMENT_POST_TEXTAREA.value, newCommentTimestampMs,
-          COMMENT_TYPE_QUESTION)
-      .then(() => {
-        loadDiscussion();
-      });
-};
-
-/**
- * Adds comments to the discussion element.
- */
-async function loadDiscussion() {
-  // Clear any existing comments before loading.
-  ELEMENT_DISCUSSION.textContent = '';
-  currentRootDiscussionComments = [];
-
-  const manager = new DiscussionManager(window.LECTURE);
-  const rootComments = await manager.fetchRootComments();
-  for (const rootComment of rootComments) {
-    const rootCommentElement = new DiscussionComment(rootComment, manager);
-    currentRootDiscussionComments.push(rootCommentElement);
-    ELEMENT_DISCUSSION.appendChild(rootCommentElement);
-  }
-}
-
-/**
- * Updates the timestamp displayed and sent by the new comment form.
- *
- * @param {number} timeMs The new time in milliseconds to use.
- */
-function updateNewCommentTimestamp(timeMs) {
-  ELEMENT_TIMESTAMP_SPAN.innerText = timestampToString(timeMs);
-  newCommentTimestampMs = timeMs;
-}
-
-/**
- * Returns an array of the `DiscussionComment`s with timestamps near
- * `timestampMs`. This returns an empty array if no elements are nearby.
- *
- * <p>A comment is nearby if it is within `TIME_TOLERANCE_MS`.
- */
-function getNearbyDiscussionComments(timestampMs) {
-  const nearby = [];
-  // currentRootDiscussionComments is already sorted by timestamp.
-  for (const element of currentRootDiscussionComments) {
-    const commentTime = element.comment.timestampMs.value;
-    if (commentTime < timestampMs - TIME_TOLERANCE_MS) {
-      // Before the start of the range, continue to next.
-      continue;
-    }
-    if (commentTime > timestampMs + TIME_TOLERANCE_MS) {
-      // Outside of range, there will be no more.
-      return nearby;
-    }
-    nearby.push(element);
-  }
-  return nearby;
+  discussion = new DiscussionArea(window.LECTURE);
+  discussion.initialize();
+  // This is used as the `onclick` handler of the new comment area submit
+  // button. It must be set after discussion is initialized.
+  window.postNewComment = discussion.postNewComment.bind(discussion);
 }
 
 /**
  * Renders a comment and its replies, with a form to post a new reply.
  */
 // TODO: PR #264 moves this to a new file.
-class DiscussionComment extends HTMLElement {
-  #manager;
+export class DiscussionComment extends HTMLElement {
+  #discussion;
 
   /**
    * Creates an custom HTML element representing a comment.  This uses the
@@ -129,11 +59,12 @@ class DiscussionComment extends HTMLElement {
    *
    * @param comment The comment from the servlet that this element should
    *     render.
-   * @param {DiscussionManager} manager The current discussion's manager.
+   * @param {DiscussionArea} discussion The discussion that this comment is a
+   *     part of.
    */
-  constructor(comment, manager) {
+  constructor(comment, discussion) {
     super();
-    this.#manager = manager;
+    this.#discussion = discussion;
     this.comment = comment;
     this.attachShadow({mode: 'open'});
     const shadow = TEMPLATE_COMMENT.content.cloneNode(true);
@@ -177,10 +108,7 @@ class DiscussionComment extends HTMLElement {
     this.shadowRoot.querySelector(SELECTOR_POST_REPLY).onclick = () => {
       // TODO: Make this its own function.
       const textarea = this.shadowRoot.querySelector(SELECTOR_REPLY_TEXTAREA);
-      this.#manager.postReply(textarea.value, this.comment.commentKey.id)
-          .then(() => {
-            loadDiscussion();
-          });
+      this.#discussion.postReply(textarea.value, this.comment.commentKey.id);
     };
   }
 
@@ -192,7 +120,7 @@ class DiscussionComment extends HTMLElement {
     const replyDiv = document.createElement('div');
     replyDiv.slot = SLOT_REPLIES;
     for (const reply of replies) {
-      replyDiv.appendChild(new DiscussionComment(reply, this.#manager));
+      replyDiv.appendChild(new DiscussionComment(reply, this.#discussion));
     }
     this.appendChild(replyDiv);
   }
@@ -224,10 +152,5 @@ customElements.define('discussion-comment', DiscussionComment);
 
 /** Seeks discussion to `timeMs`. */
 export function seekDiscussion(timeMs) {
-  updateNewCommentTimestamp(timeMs);
-  const nearbyComments = getNearbyDiscussionComments(timeMs);
-  if (nearbyComments.length == 0) {
-    return;
-  }
-  nearbyComments[0].scrollToTopOfDiscussion();
+  discussion.seek(timeMs);
 }
