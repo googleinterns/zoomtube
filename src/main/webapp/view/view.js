229832bc9340a64e59717f62f1c8cb9d6b8934a8
Jay Saleh <jaysaleh66@gmail.com>
2020-08-24 18:23:34 +0000

    Added semi

--- src/main/webapp/view/view.js
+++ src/main/webapp/view/view.js
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import DiscussionArea from './discussion/discussion-area.js'
+import DiscussionArea from './discussion/discussion-area.js';
 import TranscriptArea from './transcript/transcript-area.js';
 import Video from './video/video.js';
 
