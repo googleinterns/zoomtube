c814baa66947cbe715c1ac36e9a1b09f7af37dde
Jay Saleh <jaysaleh66@gmail.com>
2020-08-25 17:54:06 +0000

    Implemented do post

--- src/main/java/com/googleinterns/zoomtube/servlets/IconFeedbackServlet.java
+++ src/main/java/com/googleinterns/zoomtube/servlets/IconFeedbackServlet.java
@@ -17,14 +17,18 @@ package com.googleinterns.zoomtube.servlets;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
 import com.google.gson.Gson;
-import com.googleinterns.zoomtube.data.Lecture;
+import com.googleinterns.zoomtube.data.IconFeedback;
+import com.googleinterns.zoomtube.utils.IconFeedbackUtil;
 import com.googleinterns.zoomtube.utils.LectureUtil;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -34,6 +38,14 @@ import javax.servlet.http.HttpServletResponse;
 public class IconFeedbackServlet extends HttpServlet {
   private DatastoreService datastore;
 
+  private static final String PARAM_LECTURE_ID = "lectureId";
+  private static final String PARAM_TIMESTAMP = "timestampMs";
+  private static final String PARAM_ICON_TYPE = "iconType";
+
+  private static final String ERROR_MISSING_LECTURE_ID = "Missing lecture id parameter.";
+  private static final String ERROR_MISSING_TIMESTAMP = "Missing timestamp parameter.";
+  private static final String ERROR_MISSING_ICON_TYPE = "Missing icon type parameter.";
+
   @Override
   public void init() throws ServletException {
     datastore = DatastoreServiceFactory.getDatastoreService();
@@ -41,12 +53,34 @@ public class IconFeedbackServlet extends HttpServlet {
 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    long lectureId = Long.parseLong(request.getParameter("lectureId"));
-    long videoTimeStamp = Long.parseLong(request.getParameter("timestamp"));
-    String iconType = request.getParameter("iconType");
-    System.out.println(lectureId);
-    System.out.println(videoTimeStamp);
-    System.out.println(iconType);
+    Optional<String> error = validatePostRequest(request);
+    if (error.isPresent()) {
+      response.sendError(HttpServletResponse.SC_BAD_REQUEST, error.get());
+      return;
+    }
+
+    long lectureId = Long.parseLong(request.getParameter(PARAM_LECTURE_ID));
+    long videoTimeStamp = Long.parseLong(request.getParameter(PARAM_TIMESTAMP));
+    IconFeedback.Type type = IconFeedback.Type.valueOf(request.getParameter(PARAM_ICON_TYPE));
+    Key lectureEntityKey = KeyFactory.createKey(LectureUtil.KIND, lectureId);
+
+    Entity iconFeedbackEntity =
+        IconFeedbackUtil.createEntity(lectureEntityKey, videoTimeStamp, type);
+    System.out.println(iconFeedbackEntity);
     System.out.println();
+    datastore.put(iconFeedbackEntity);
+  }
+
+  private Optional<String> validatePostRequest(HttpServletRequest request) {
+    if (request.getParameter(PARAM_LECTURE_ID) == null) {
+      return Optional.of(ERROR_MISSING_LECTURE_ID);
+    }
+    if (request.getParameter(PARAM_TIMESTAMP) == null) {
+      return Optional.of(ERROR_MISSING_TIMESTAMP);
+    }
+    if (request.getParameter(PARAM_ICON_TYPE) == null) {
+      return Optional.of(ERROR_MISSING_ICON_TYPE);
+    }
+    return Optional.empty();
   }
 }
