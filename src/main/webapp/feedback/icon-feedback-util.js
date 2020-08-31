969ad1571c6fd9ad76ecb2d56132059c0efc70c8
jaysaleh <47684906+jaysaleh@users.noreply.github.com>
2020-08-31 15:44:45 -0700

    Update src/main/webapp/feedback/icon-feedback-util.js
    Co-authored-by: Amy Nguyen <35519361+amuamushu@users.noreply.github.com>

--- src/main/webapp/feedback/icon-feedback-util.js
+++ src/main/webapp/feedback/icon-feedback-util.js
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/** Contains conts related to IconFeedback. */
+/** Contains counts related to IconFeedback. */
 export default class IconFeedbackUtil {
   static TYPE_GOOD = 'GOOD';
   static TYPE_BAD = 'BAD';
