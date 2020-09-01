e55d403917d5662c4630afd7bc91862e32e1e270
Amy Nguyen <amytn@google.com>
2020-09-01 07:58:13 +0000

    Remove unused imports.

--- src/test/java/com/googleinterns/zoomtube/transcriptParser/TranscriptParserTest.java
+++ src/test/java/com/googleinterns/zoomtube/transcriptParser/TranscriptParserTest.java
@@ -20,10 +20,8 @@ import static org.junit.Assert.fail;
 
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
-import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.KeyFactory;
-import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.datastore.Query.Filter;
 import com.google.appengine.api.datastore.Query.FilterOperator;
